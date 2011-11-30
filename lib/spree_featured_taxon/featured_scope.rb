4ae77cb8ecc24761f99abfc079d8998859be4381
George F Murphy <george@matmon.com>
2011-11-30 16:47:20 -0600

    add featured_taxons helper method to Spree::BaseController

--- lib/spree_featured_taxon/featured_scope.rb
+++ lib/spree_featured_taxon/featured_scope.rb
@@ -1,6 +1,7 @@
 module SpreeFeaturedTaxon
   class FeaturedScope
     include Enumerable
+
     delegate :size, :each, :to => :sampled_scope
     attr_reader :sample_size
 
@@ -9,12 +10,22 @@ module SpreeFeaturedTaxon
       @sample_size = sample_size
     end
 
+    def scope
+      @scope.clone
+    end
+
     def limit(number)
       FeaturedScope.new(@scope.limit(number), sample_size)
     end
 
     def sample(size)
       FeaturedScope.new(@scope, size)
+    end
+
+    def ==(other)
+      @scope == other.scope && @sample_size == other.sample_size
+    rescue NoMethodError
+      false
     end
 
     private
