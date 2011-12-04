892484adb0df5914305e2fe569332582b921a7c6
George F Murphy <gfmurphy@gmail.com>
2011-12-03 21:32:58 -0600

    instance eval featured scope cutomization instead of yielding scope to caller

--- lib/spree_featured_taxon/featured_taxon_support.rb
+++ lib/spree_featured_taxon/featured_taxon_support.rb
@@ -8,7 +8,9 @@ module SpreeFeaturedTaxon
     # spree controllers
     def featured_taxon_scope(&b)
       @featured_taxon_scope ||= Taxon.featured
-      @featured_taxon_scope = yield @featured_taxon_scope if block_given?
+      if block_given?
+        @featured_taxon_scope = @featured_taxon_scope.instance_eval &b
+      end
       @featured_taxon_scope
     end
 
@@ -16,8 +18,10 @@ module SpreeFeaturedTaxon
     # can further customize scope that is yielded to provided block
     def featured_taxons(&b)
       scope = featured_taxon_scope.clone
-      scope = yield scope if block_given?
-      FeaturedScope.new(scope)
+      if block_given?
+        scope = scope.instance_eval &b
+      end
+      FeaturedScope.new scope
     end
   end
 end
